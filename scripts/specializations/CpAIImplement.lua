4d33b001347b21ad0424417c7e7d204e1e2f7361
David Schwietering <d-schwietering@gmx.de>
2025-04-13 02:18:03 +0200

    Add debug info

--- scripts/specializations/CpAIImplement.lua
+++ scripts/specializations/CpAIImplement.lua
@@ -24,6 +24,21 @@ end
 
 function CpAIImplement.registerEventListeners(vehicleType)
     SpecializationUtil.registerEventListener(vehicleType, 'onLoad', CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementStart", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementActive", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementEnd", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementPrepareForWork", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementStartLine", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementEndLine", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementStartTurn", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementTurnProgress", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementEndTurn", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementSideOffsetChanged", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementBlock", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementContinue", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementPrepareForTransport", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementJobVehicleBlock", CpAIImplement)
+    SpecializationUtil.registerEventListener(vehicleType, "onAIImplementJobVehicleContinue", CpAIImplement)
 end
 
 function CpAIImplement.registerFunctions(vehicleType)
@@ -41,4 +56,109 @@ end
 function CpAIImplement:onLoad(savegame)
     self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
     local spec = self.spec_cpAIImplement
+end
+
+function CpAIImplement:onAIImplementStart()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementStart()")
+end
+
+function CpAIImplement:onAIImplementActive()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    -- CpUtil.info("onAIImplementActive()")
+end
+
+function CpAIImplement:onAIImplementEnd()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementEnd()")
+end
+
+function CpAIImplement:onAIImplementPrepareForWork()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementPrepareForWork()")
+end
+
+function CpAIImplement:onAIImplementStartLine()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementStartLine()")
+end
+
+function CpAIImplement:onAIImplementEndLine()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementEndLine()")
+end
+
+function CpAIImplement:onAIImplementStartTurn()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementStartTurn()")
+end
+
+function CpAIImplement:onAIImplementTurnProgress()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    -- CpUtil.info("onAIImplementTurnProgress()")
+end
+
+function CpAIImplement:onAIImplementEndTurn()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementEndTurn()")
+end
+
+function CpAIImplement:onAIImplementSideOffsetChanged()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementSideOffsetChanged()")
+end
+
+function CpAIImplement:onAIImplementBlock()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementBlock()")
+end
+
+function CpAIImplement:onAIImplementContinue()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementContinue()")
+end
+
+function CpAIImplement:onAIImplementPrepareForTransport()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementPrepareForTransport()")
+end
+
+function CpAIImplement:onAIImplementJobVehicleBlock()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementJobVehicleBlock()")
+end
+
+function CpAIImplement:onAIImplementJobVehicleContinue()
+    self.spec_cpAIImplement = self["spec_" .. CpAIImplement.SPEC_NAME]
+    local spec = self.spec_cpAIImplement
+
+    CpUtil.info("onAIImplementJobVehicleContinue()")
 end
