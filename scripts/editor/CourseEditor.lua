56d2b0505b5d1dd64492694d4423f7ea3aa08932
David Schwietering <d-schwietering@gmx.de>
2025-04-25 11:50:42 +0200

    Fix for course editor lane offset
    #795

--- scripts/editor/CourseEditor.lua
+++ scripts/editor/CourseEditor.lua
@@ -167,7 +167,6 @@ function CourseEditor:onClickLaneOffsetSetting(closure, ignoreDialog)
 				if item > 0 then
 					local value = allowedValues[item]
 					self.courseWrapper:getCourse():setPosition(value)
-					self.courseDisplay:setCourse(self.courseWrapper)
 					closure(texts[item])
 				end
 			end,
