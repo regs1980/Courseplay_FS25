9420d76a495c66d1c61c094cf9ea5ee21e83126e
Peter Vaiko <github@vaiko.us>
2025-11-07 17:45:12 -0500

    test: trace AI events

--- scripts/ai/tasks/CpAITaskFieldWork.lua
+++ scripts/ai/tasks/CpAITaskFieldWork.lua
@@ -61,6 +61,19 @@ end
 
 --- Makes sure the cp fieldworker gets started.
 function CpAITaskFieldWork:start()
+
+	self.vehicle.raiseAIEvent = function(vehicle, event1, event2, ...)
+		CpUtil.infoVehicle(vehicle, "raiseAIEvent %s %s", event1, event2)
+		AIVehicle.raiseAIEvent(vehicle, event1, event2, ...)
+	end
+
+	if self.vehicle.actionController ~= nil then
+		self.vehicle.actionController.onAIEvent = function(actionController, sourceVehicle, eventName)
+			CpUtil.infoVehicle(self.vehicle, "   onAIEvent %s, source %s", eventName, CpUtil.getName(sourceVehicle))
+			VehicleActionController.onAIEvent(actionController, sourceVehicle, eventName)
+		end
+	end
+	
 	self:debug("Field work task started.")
 	local spec = self.vehicle.spec_aiFieldWorker
 	spec.isActive = true
